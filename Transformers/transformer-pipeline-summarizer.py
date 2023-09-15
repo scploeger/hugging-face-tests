7db221bb77ac77638adb42e9a52ebdeb5ef4f49c
scploeger <spenceinvents@hotmail.com>
2023-09-15 15:13:33 -0400

    modified trasnformer pipeline summarizer input text

--- Transformers/transformer-pipeline-summarizer.py
+++ Transformers/transformer-pipeline-summarizer.py
@@ -2,7 +2,7 @@ from transformers import pipeline
 
 summarizer = pipeline("summarization", model="facebook/bart-large-cnn")
 
-text = """Thank you for submitting your application for the Analyst, Master Data Management position at Company Inc..
+text = """Thank you for submitting your application for the Analyst, Master Data Management position at our company.
 After reviewing your profile, we regret to inform you that your application was not selected for the next steps of our hiring process for the role you applied for.
 """
 
